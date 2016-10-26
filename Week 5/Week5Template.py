c030e69e4ea34891b7a0edef94de0dfa8cd9eb3f
Gianluca Gordon <ggordon173@gmail.com>
2016-10-26 11:50:16 +0100

    todays work

--- Week 5/Week5Template.py
+++ Week 5/Week5Template.py
@@ -14,4 +14,65 @@ def doomsday(y):
     >>> type(doomsday(2010))
     <class 'int'>
     """
-    
+
+    if 1800 < y < 1900:
+
+        x=5
+        w = y % 100
+        a = w // 12
+        b = w % 12
+        c = b // 4
+        d = (a + b + c) % 7
+        e = x + d
+        if e > 6:
+            e -= 7
+
+        return e
+
+    elif 1900 < y < 2000:
+
+        x=3
+        w = y % 100
+        a = w // 12
+        b = w % 12
+        c = b // 4
+        d = (a + b + c) % 7
+        e = x + d
+        if e > 6:
+            e -= 7
+
+        return e
+
+    elif 2000 < y < 2100:
+
+        x=2
+        w = y % 100
+        a = w // 12
+        b = w % 12
+        c = b // 4
+        d = (a + b + c) % 7
+        e = x + d
+        if e > 6:
+            e -= 7 
+
+        return e
+
+    elif 2100 < y < 2200:
+
+        x=0
+        w = y % 100
+        a = w // 12
+        b = w % 12
+        c = b // 4
+        d = (a + b + c) % 7
+        e = x + d
+        if e > 6:
+            e -= 7 
+
+        return e
+
+    else:
+        e = -1
+
+        return e
+
