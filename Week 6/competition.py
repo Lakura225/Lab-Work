e6a90a8ac9bcb231407a60dbbd69ca3ce900cbdb
Luca Gordon <ggordon173@gmail.com>
2016-11-11 12:47:38 +0000

    week 7 and crap

--- Week 6/competition.py
+++ Week 6/competition.py
@@ -1,8 +1,13 @@
 def lightss():
 	lights = [0] * 50
 	i=0
+	n=0
 	while i <= 50:
 		for times in range(1,50):
 			n=n*times
-			for n in enumerate(lights):
-				if lights
+			for n,l in enumerate(lights):
+				if l == 0:
+					lights[l] == 1
+				elif l == 1:
+					lights[l] == 0
+	print(lights)
