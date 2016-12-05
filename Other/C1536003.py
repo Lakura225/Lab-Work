1fc79426a79899604c6000c4d2726eeb6d7f1436
Gianluca Gordon <ggordon173@gmail.com>
2016-12-05 12:28:01 +0000

    zsdcfzasc

--- Other/C1536003.py
+++ Other/C1536003.py
@@ -5,7 +5,7 @@ def game(ra, rb):
     probA = ra / (ra + rb)
     scoreA = 0
     scoreB = 0
-    #random.seed(57)
+    # random.seed(57)
     while True:
         if scoreA >= 11 and scoreA - scoreB >= 2:
             return scoreA, scoreB
@@ -20,7 +20,24 @@ def game(ra, rb):
             else:
                 scoreB += 1
 
+
 def winProbability(ra, rb, n):
+    Awins = 0
     for i in range(n):
-        game(ra,rb)
-        
+        probA = ra / (ra + rb)
+        scoreA = 0
+        scoreB = 0
+        # random.seed(57)
+        for f in range(n):
+            if scoreA >= 11 and scoreA - scoreB >= 2:
+                Awins += 1
+                scoreA = 0
+                scoreB = 0
+            else:
+                rng = random.random()
+                if rng < probA:
+                    scoreA += 1
+                else:
+                    scoreB += 1
+    prob = Awins / n
+    print("{0:.2f}".format(prob))
